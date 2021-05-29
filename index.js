e2d111a28d7c042486f5fcd005f6c439f92ccfed
Vitamarys <vitaliy.marysiy@gmail.com>
2021-05-29 12:48:28 +0300

    weathr

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-const {cpu} =  require('./module/cpu.js');
+// const {cpu} =  require('./module/cpu.js');
 
 
-console.log(cpu());
+
+
+const axios = require('axios');
+const fs = require("fs");
+
+const url = 'http://api.openweathermap.org/data/2.5/weather?id=703448&appid=bbbc8e17bf070c27202f44f3e71c04e6';
+
+async function weahter(){
+    const  response = await axios.get(url);
+    const data =  await response.data;
+    const  temp =  data.main.temp - 273;
+    const date = Date(data.dt).toString()
+    fs.writeFileSync("result.txt", `Дата: ${date}, температура: ${temp}`)
+  
+
+}
+
+
+setInterval(weahter,10000)
+
