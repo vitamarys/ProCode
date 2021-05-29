6c436fda09f2147a417b28a5c189e292f2d07df5
Vitamarys <vitaliy.marysiy@gmail.com>
2021-05-29 14:21:55 +0300

    tommrow weather

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-// const {cpu} =  require('./module/cpu.js');
+
 
 
 
 
 const axios = require('axios');
 const fs = require("fs");
-
-const url = 'http://api.openweathermap.org/data/2.5/weather?id=703448&appid=bbbc8e17bf070c27202f44f3e71c04e6';
+const url = 'http://api.openweathermap.org/data/2.5/forecast/?q=Kyiv&units=metric&cnt=6&appid=bbbc8e17bf070c27202f44f3e71c04e6';
 
 async function weahter(){
     const  response = await axios.get(url);
-    const data =  await response.data;
-    const  temp =  data.main.temp - 273;
-    const date = Date(data.dt).toString()
-    fs.writeFileSync("result.txt", `Дата: ${date}, температура: ${temp}`)
+     const data =  await response.data;
+     const list =  await data.list;
+     const tomorrow = list[5];
+     const  date = tomorrow.dt_txt;
+     const temp = tomorrow.main.temp;
+    
+
+       
+        fs.appendFile('result.txt', `Дата: ${date}, температура: ${temp} \n`, err => {
+            if (err) {
+              console.error(err)
+              return
+            }
+            
+          })
+        
+  
+   
+
   
 
 }
 
-
-setInterval(weahter,900000)
+weahter();
+ setInterval(weahter,900000)
 
