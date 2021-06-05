020fb08c7d2ce1bad04d257b18b49bd873c622ad
Vitalii <vitalii.marusii@gmail.com>
2021-06-05 06:53:25 +0300

    Change Http

--- http/index.js
+++ http/index.js
@@ -1,26 +1,15 @@
 const http = require('http');
 const fs = require("fs");
 const axios = require('axios');
-const url = 'https://api.unsplash.com/search/photos?query=minimal █';
-http.createServer((req,res)=>{
-    //запрос от браузера
-    console.log(req.headers);
-    //отправка файла на запрос
-  
-    console.log(`Запрошенный адрес: ${req.url}`);
-    const filePath = req.url.substr(1);
-    fs.readFile(filePath, function(error, data){
-              
-        if(error){
-                  
-            res.statusCode = 404;
-            res.end("Resourse not found!");
-        }   
-        else{
-            res.end(data);
-        }
-    });
+const url = 'https://static.wikia.nocookie.net/shararam-smeshi/images/6/6e/%D0%A4%D0%BE%D0%BD_%C2%AB%D0%A4%D0%BB%D0%B0%D0%BC%D0%B8%D0%BD%D0%B3%D0%BE%C2%BB.png/revision/latest/scale-to-width-down/700?cb=20190308202249&path-prefix=ru';
 
+
+
+http.createServer((req,res)=>{
+    
+    const stream = fs.createReadStream(__dirname + '/img.jpg');
+    stream.pipe(res);
+   
     
 }).listen(8000)
 
