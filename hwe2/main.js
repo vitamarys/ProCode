b998612cb56d54eea3efa4617afd04309abe3582
Vitamarys <vitaliy.marysiy@gmail.com>
2021-06-16 22:38:27 +0300

    'hwe2'

---
+++ hwe2/main.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const mainRoure = require('./routes/main.js')
+
+
+
+const server  = express();
+
+server.set('views', path.join(__dirname,'views'));
+server.set('view engine', 'ejs')
+
+
+server.get('/', mainRoure)
+
+server.listen(8000);
