65e5d9935404733f061e9df80770c13a2e949752
Vitalii <vitalii.marusii@gmail.com>
2021-06-28 13:54:10 +0300

    'home3'

--- hwe3/routes/test.js
+++ hwe3/routes/test.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const multer = require('multer');
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
 const router = express.Router();
 
 
@@ -10,8 +11,10 @@ const storage = multer.diskStorage({
     filename:function(req, file, cb){
         const {name} = req.body;
       
-        cb(null, name + '-' + Date.now());
+        cb(null, name + '-' + Date.now() + path.extname(file.originalname) );
+        
     }
+   
 })
 
 const upload = multer({ 
@@ -20,11 +23,34 @@ const upload = multer({
  });
  
 
-router.post('/',upload.single('load'),(req,res)=>{
+router.post('/',upload.single('load'), async (req,res)=>{
+    const { filename: image } = req.file;
+    await sharp(req.file.path)
+    
+    
+    .jpeg({ quality: 100 })
+    .toFile(
+        path.resolve(req.file.destination,'./orig', image)
+    )
+    await sharp(req.file.path)
+       .resize(200, 200)
+    .jpeg({ quality: 90 })
+    .greyscale()
+    .toFile(
+        path.resolve(req.file.destination,'./resized', image)
+    )
+    await sharp(req.file.path)
    
+    .toFormat('jpeg')
+    .toFile(
+        path.resolve(req.file.destination,'./format', image)
+    )
+    fs.unlinkSync(req.file.path)
     console.log(req.file);
     res.json({status:'ok'})
    
+   
 });
+
 
 module.exports = router;
